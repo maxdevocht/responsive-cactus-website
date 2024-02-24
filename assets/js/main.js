c40740c24720055773ed0badf8552bcb0a7e147c
Max de Vocht <maxdevocht@hotmail.com>
2024-02-24 15:01:44 +0100

    Update main.js
    Coded remove mobile nav when clicked on link

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,12 @@ if (navClose) {
         navMenu.classList.remove('show-menu')
     })
 }
+
+/* remove nav bar when clicked on link */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
