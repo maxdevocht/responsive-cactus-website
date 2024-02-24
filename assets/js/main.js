f13f100a54214fd10932119937153cdb7900120a
Max de Vocht <maxdevocht@hotmail.com>
2024-02-24 16:30:46 +0100

    Update js
    change of background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,11 @@ const linkAction = () => {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* change background header */
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
