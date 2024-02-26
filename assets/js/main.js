adf55c51623c7b333d676a7dab917456b63fb404
Max de Vocht <maxdevocht@hotmail.com>
2024-02-26 13:22:24 +0100

    Update js
    Added scroll up function

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,11 @@ const blurHeader = () =>{
                        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+/* show scroll up  */
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
